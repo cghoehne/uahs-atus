96d5b2120b1f902f33ba5385dc925e0a501e0fbc
Chris Hoehne <31117250+cghoehne@users.noreply.github.com>
2018-10-19 12:49:42 -0700

    more efficient multi-timezone assignment using data.table. in-progress more efficient activity-weather time matching using data.table (works on small subset but needs to handle large time diffs)

--- data-prep.R
+++ data-prep.R
@@ -7,7 +7,7 @@ memory.limit(size = 56000)
 
 # list of all dependant packages
 list.of.packages <- c("lubridate",
-                      "chron",
+                      "weathermetrics",
                       "tidyverse",
                       "data.table", 
                       "here")
@@ -24,11 +24,8 @@ lapply(list.of.packages, library, character.only = TRUE)
 # merged on activities for all ATUS respondants in MSA sample
 atus1 <- readRDS(here("data/atus1_filtered_0315.rds"))
 atus2 <- readRDS(here("data/atus2_filtered_0315.rds"))
-atusnew <- rbind(atus1,atus2)
+atus <- rbind(atus1,atus2)
 rm(atus1,atus2)
-
-saveRDS(atus[1:floor(nrow(atus)/2)], here("data/atus1_filtered_0315.rds"))
-saveRDS(atus[(floor(nrow(atus)/2)+1):nrow(atus)], here("data/atus2_filtered_0315.rds"))
 
 # load list of metros for indexing
 metros <- readRDS(here("data/msa_list.rds"))
@@ -40,104 +37,62 @@ w.data <- readRDS(here("data/ncdc_filtered_0315.rds"))
 loc.table <- readRDS(here("data/location_table.rds"))
 act.table <- readRDS(here("data/activity_table.rds"))
 
-# Recode activity start and stop time variables to minutes. 0 = 0:00:00, 1439.983 = 11:59:59.
-atus$STARTMIN <- 60 * 24 * as.numeric(times(atus$TUSTARTTIM))
-atus$STOPMIN <- 60 * 24 * as.numeric(times(atus$TUSTOPTIME))
+# Creates a date + time column as "YYYY-MM-DD HH:MM:SS TZ"
+atus$DateTimeStart <- ymd_hms(paste(atus$TUDIARYDATE,atus$TUSTARTTIM))
+atus$DateTimeEnd <- ymd_hms(paste(atus$TUDIARYDATE,atus$TUSTOPTIME))
 
-# Creates a date + time column as YYYY-MM-DD HH:MM, R recognizes these values as time. 
-atus$DateTime<- format(strptime(paste(atus$TUDIARYDATE,atus$TUSTARTTIM,sep =""), format="%Y%m%d%H:%M"), format="%Y-%m-%d %H:%M")
+# rename MSA IDs to "ID" for matching
+setnames(atus, "METFIPS", "ID")
+setnames(w.data, "station", "ID")
 
-# Add TU Day of month variable
-atus$TUDAY <- format(strptime(atus$TUDIARYDATE, format="%Y%m%d"), format="%d")
-
-# Add TU midpoint time (in minutes from beginning of day). This represents the midpoint time of the activity
-atus$MIDTIME <- (atus$STARTMIN + atus$STOPMIN)/2
-
-# Creates a date + time column with the starting, midpoint, and ending activity timestamp
-atus$DateTimeStart <- format(paste(strptime(atus$TUDIARYDATE, format="%Y%m%d"),times(atus$STARTMIN / 1440),sep=" "), format="%Y-%m-%d %H:%M:%S")
-atus$DateTimeStart <- as.POSIXct(atus$DateTimeStart)
-
-atus$DateTimeMid.f <- format(paste(strptime(atus$TUDIARYDATE, format="%Y%m%d"),times((((atus$MIDTIME - atus$STARTMIN) / 2 ) + atus$STARTMIN )/ 1440) ,sep=" "), format="%Y-%m-%d %H:%M:%S")
-atus$DateTimeMid.f <- as.POSIXct(atus$DateTimeMid.f)
-
-atus$DateTimeMid <- format(paste(strptime(atus$TUDIARYDATE, format="%Y%m%d"),times(atus$MIDTIME / 1440),sep=" "), format="%Y-%m-%d %H:%M:%S")
-atus$DateTimeMid <- as.POSIXct(atus$DateTimeMid)
-
-atus$DateTimeMid.b <- format(paste(strptime(atus$TUDIARYDATE, format="%Y%m%d"),times((atus$STOPMIN - ((atus$STOPMIN - atus$MIDTIME) / 2 )) / 1440) ,sep=" "), format="%Y-%m-%d %H:%M:%S")
-atus$DateTimeMid.b <- as.POSIXct(atus$DateTimeMid.b)
-
-atus$DateTimeEnd <- format(paste(strptime(atus$TUDIARYDATE, format="%Y%m%d"),times(atus$STOPMIN / 1440),sep=" "), format="%Y-%m-%d %H:%M:%S")
-atus$DateTimeEnd <- as.POSIXct(atus$DateTimeEnd)
-
-
-# create dummy temp variables for later
-atus$temp <- NA   # new apparent temp variable
-atus$TempC <- NA     # temp in C variable
-atus$DewpC <- NA     # dewpt in C variable
+# calucate mid point time and the quarter and 3 quarters times (for calculating avg temp during act)
+atus$DateTimeMid <- atus$DateTimeEnd + (difftime(atus$DateTimeEnd, atus$DateTimeStart, units = "mins") * 0.50)
+atus$DateTimeMid.f <- atus$DateTimeEnd + (difftime(atus$DateTimeEnd, atus$DateTimeStart, units = "mins") * 0.25)
+atus$DateTimeMid.b <- atus$DateTimeEnd + (difftime(atus$DateTimeEnd, atus$DateTimeStart, units = "mins") * 0.75)
 
 # create a Year-Month-Day and a Year-Month variable to use for matching with temps
 atus$YrMthDay <- format(strptime(atus$DateTimeMid, format="%Y-%m-%d %H:%M:%S"), format="%Y-%m-%d")
 atus$YrMth <- format(strptime(atus$DateTimeMid, format="%Y-%m-%d %H:%M:%S"), format="%Y-%m")
 
-# Remove activities with a missing or invalid timestamp
-atus <- subset(atus, !is.na(atus$DateTimeMid))
+# Remove activities with a missing timestamp
+atus <- atus[!is.na(DateTimeMid)]
+
+# hour start and end for matching
 atus$hr.s <- as.numeric(format(strptime(atus$DateTimeStart, format="%Y-%m-%d %H:%M:%S"), format="%H"))
 atus$hr.e <- as.numeric(format(strptime(atus$DateTimeEnd, format="%Y-%m-%d %H:%M:%S"), format="%H"))
 
 # income: HEFAMINC if year < 2010, HUFAMINC if year >= 2010
 atus$Income <- NA
-
-for(b in 1:nrow(atus)){
-  if(atus$HUFAMINC[b] < 0){
-    atus$Income[b] <- atus$HEFAMINC[b]
-  } 
-  if(atus$HEFAMINC[b] < 0){
-    atus$Income[b] <- atus$HUFAMINC[b]
-  } 
-}
-
-# remvoe any other extraneous variables
-
+atus[HUFAMINC < 0, Income := atus$HEFAMINC]
+atus[HEFAMINC < 0, Income := atus$HUFAMINC]
 
 #~<@>~~<@>~<@>~<@>~<@>~<@>~<@>~<@>~<@>~<@>~#
 #    Match temps to activities by MSA      #
 #~<@>~~<@>~<@>~<@>~<@>~<@>~<@>~<@>~<@>~<@>~#
 
-# split atus data into dt list by MSA
-atus <- merge(atus, metros[, .(MSA_code,abbr)], by.x = "METFIPS", by.y = "MSA_code")
-atus <- as.data.table(atus)
-atus.l <- split(atus, METFIPS)
-
+# change timezone to be correct for each MSA (cause respondants reprot local time)
+# must split and assign because can't assign vectorized timezones (lame!)
+atus <- merge(atus, metros[, .(abbr,Timezone)], by = "abbr")
+atus.l <- split(atus, as.factor(atus$ID)) # split atus data into dt list by MSA
+for(a in 1:length(atus.l)){
+  atus.l[[a]][, DateTimeStart := with_tz(DateTimeStart, tzone = unique(atus.l[[a]]$Timezone))]
+  atus.l[[a]][, DateTimeEnd := with_tz(DateTimeEnd, tzone = unique(atus.l[[a]]$Timezone))] 
+}
+atus <- rbindlist(atus.l) # rebind list of DTs
 
 # formating NCDC weather data
-w.data$YrMthDay <- format(strptime(w.data$DateTime, format="%Y-%m-%d %H:%M:%S"), format="%Y-%m-%d")
 w.data$hr <- as.numeric(format(strptime(w.data$DateTime, format="%Y-%m-%d %H:%M:%S"), format="%H"))
 
 # Calc relative humidity
 w.data$rh <- exp((17.625*w.data$DEWPC)/(243.04+w.data$DEWPC))/exp((17.625*w.data$TEMPC)/(243.04+w.data$TEMPC))
 
 # calculate Heat Index via NWS equations
-w.data$rh <- w.data$rh*100
-w.data$TEMP <- w.data$TEMPC * 9/5 + 32
-w.data$A <- -10.3 + 1.1*w.data$TEMP + 0.047*w.data$rh
-w.data$B <- (-42.379 + 2.04901523*w.data$TEMP
-         + 10.14333127*w.data$rh - 0.22475541*w.data$TEMP*w.data$rh 
-         - 0.00683783*w.data$TEMP*w.data$TEMP - 0.05481717*w.data$rh*w.data$rh 
-         + 0.00122874*w.data$TEMP*w.data$TEMP*w.data$rh + 0.00085282*w.data$TEMP*w.data$rh*w.data$rh 
-         - 0.00000199*w.data$TEMP*w.data$TEMP*w.data$rh*w.data$rh)
+w.data[, heat := heat.index(t = TEMP, dp = DEWP, temperature.metric = "fahrenheit", output.metric = "fahrenheit", round = 0)]
+w.data[, temp := heat]
+w.data[heat < 80, temp := TEMP] # use air temp if below 80 F
 
-w.data$heat <- ifelse(w.data$TEMP <= 40, w.data$TEMP,
-                  ifelse(w.data$A < 79, w.data$A,
-                         ifelse(w.data$rh <= 0.13 & w.data$TEMP >= 80 & w.data$TEMP <= 112, w.data$B - ((13-w.data$rh)/4)*SQRT((17-ABS(w.data$TEMP-95.))/17),
-                                ifelse(w.data$rh > 0.85 & w.data$TEMP >= 80 & w.data$TEMP <= 87, w.data$B +0.02*(w.data$rh - 85)*(87 - w.data$TEMP), w.data$B))))
-
-w.data$rh <- w.data$rh/100 # re-adjust relative humidity
-
-# use non adjustment if below 80 based on NWS 
-w.data$heat2 <- 0.5 * (w.data$TEMP + 61 + ((w.data$TEMP - 68)*1.2) + (w.data$rh*0.094))
-w.data$heat2 <- (w.data$TEMP + w.data$heat2)/2
-w.data$temp <- ifelse(w.data$heat2 >= 80, w.data$heat, w.data$heat2)   # use temp if app temp, use heat if heat index (because 'heat' only overwrote 'temp').
-w.data$temp <- (w.data$temp - 32) * 5/9 # convert to Celcius
+# correct datetime format
+w.data$DateTime <- ymd_hms(w.data$DateTime)
 
 # fill in missing hourly temps with avg for that hour in +/- 30 days
 for(i in 1:dim(w.data)[1]){
@@ -253,13 +208,6 @@ for(j in 3){  #1:dim(metros)[1]
   dt$temp4 <- NA
   dt$temp5 <- NA
   
-  # change timezone to be correct for each MSA 
-  dt$DateTimeStart <- with_tz(dt$DateTimeStart, tz = metros$Timezone[j]) 
-  dt$DateTimeMid.f <- with_tz(dt$DateTimeMid.f, tz = metros$Timezone[j]) 
-  dt$DateTimeMid <- with_tz(dt$DateTimeMid, tz = metros$Timezone[j]) 
-  dt$DateTimeMid.b <- with_tz(dt$DateTimeMid.b, tz = metros$Timezone[j]) 
-  dt$DateTimeEnd <- with_tz(dt$DateTimeEnd, tz = metros$Timezone[j]) 
-  
   # split inside/outside files
   dt.out <- dt[dt$outdoors == 1]
   dt.in <- dt[dt$outdoors == 0]
@@ -302,7 +250,24 @@ for(a in 2){ # 1:dim(metros)[1]
 t9 <- Sys.time()
 
 difftime(t9,t0, units = "hours") 
-                             
+
+
+
+# test matching
+atus.t <- atus[ID %in% c(10740,46060)]
+atus.t[, DateTime := DateTimeMid]
+atus.t <- atus.t[, .(DateTime,ID)]
+w.data.t <- w.data[, .(DateTime,ID,TEMP)]
+w.data.t[, ID := as.integer(as.character(ID))]
+
+setkey(atus.t, ID, DateTime) # setkey as ID and DateTime
+setkey(w.data.t, ID, DateTime)[, t.Date.Time := DateTime] # same but also include a new named DateTime var to keep in matched
+
+atus.m <- w.data.t[atus.t, roll = 'nearest']
+atus.m[, time.dif := difftime(DateTime,t.Date.Time, units = "mins")]
+
+
+                     
 # NOTES:
 #   The person selected to be interviewed for ATUS (i.e. the respondent) is always TULINENO = 1 where it exists.
 #   Because only one person is interviewed per household, each TUCASEID on the Activity, Respondent, and Summary file identifies a unique respondent.
